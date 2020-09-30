54d22e3876af9c5893ed225ed2a9e9885757f89a
Adrian Zimmermann <zadrianactive@gmail.com>
2020-09-30 22:47:30 +0200

    open sdl window

--- Source/Application/main.swift
+++ Source/Application/main.swift
@@ -1,1 +1,25 @@
+@_implementationOnly
+import CSDL2
+
 print("IT WORKS")
+
+SDL_Init(SDL_INIT_VIDEO);
+
+let window = SDL_CreateWindow(
+    "SDL2Test",
+    0,
+    0,
+    640,
+    480,
+    0
+);
+
+let renderer = SDL_CreateRenderer(window, -1, UInt32(SDL_RENDERER_SOFTWARE.rawValue));
+SDL_SetRenderDrawColor(renderer, 0, 0, 0, UInt8(SDL_ALPHA_OPAQUE));
+SDL_RenderClear(renderer);
+SDL_RenderPresent(renderer);
+
+SDL_Delay(3000);
+
+SDL_DestroyWindow(window);
+SDL_Quit();
